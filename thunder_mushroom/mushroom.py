b1162756a4e86d38149434b43a503a35e8c43ba6
lookuss <38791547+lookuss@users.noreply.github.com>
2021-09-09 03:55:04 +0900

    Update mushroom.py

--- thunder_mushroom/mushroom.py
+++ thunder_mushroom/mushroom.py
@@ -10,6 +10,8 @@ def load_url_list():
     pass
 
 def check_url_list():
+    print("Periodic inspection starta at 00:00")
+    print("Check url list for mushroom")
     global url_list
     tmp = []
     for url in url_list:
@@ -23,10 +25,12 @@ def run():
     global url_list
     mushroom_list = []
     for url in url_list:
+
         mushroom = mushroom_spore(url)
         mushroom_list.append(mushroom)
 
     for mushroom in mushroom_list:
+        print("Start Scan for " + mushroom.url)
         mushroom.run_all()
 
 schedule.every().day.at("00:00").do(check_url_list)
