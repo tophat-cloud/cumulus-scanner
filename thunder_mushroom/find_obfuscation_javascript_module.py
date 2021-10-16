679a504ff56595d4a5dce17b47b18fb065df170a
lookuss <ykm990625@gmail.com>
2021-10-16 16:52:51 +0900

    find_obfuscation_javascript_module.py 수정

--- thunder_mushroom/find_obfuscation_javascript_module.py
+++ thunder_mushroom/find_obfuscation_javascript_module.py
@@ -7,14 +7,15 @@ def find_obfuscation_javascript(project_id, list_of_page, list_of_html_source):
         java_script = BeautifulSoup(page_html, "lxml")
         java_script = java_script.select("script")
         body = ""
-        num = 1
+        num = 0
         if len(java_script) != 0 :
             
             for script in java_script:
-                
-                val = script.find("\n")
+                num += 1
+                val = str(script).find("\n")
                 if val == -1 or val == 0:
                     body += "Check please " +str(num) + "script\n"
                     body += "It seems to be Unobfuscated Java script code\n\n"
+                    
         request_thunder("Unobfuscated Java script", project_id, body, list_of_page[list_of_html_source.index(page_html)], 1)        
 
